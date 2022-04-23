dc21ef5dc3c923e9dfa340abb08f948eb2e7ac2f
ANWAR YUSUF <anwaryusuf@365.telkomuniversity.ac.id>
2022-04-23 14:40:21 +0700

    menambahkan API Movies.

---
+++ Modul8_1302194032/Modul8_1302194032/Controller/MoviesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Modul8_1302194032.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            // GET: api/<MoviesController>
+            [HttpGet]
+            public IEnumerable<string> Get()
+            {
+                return new string[] { "value1", "value2" };
+            }
+
+            // POST api/<MoviesController>
+            [HttpPost]
+            public void Post([FromBody] string value)
+            {
+            }
+
+            // GET api/<MoviesController>/5
+            [HttpGet("{id}")]
+            public string Get(int id)
+            {
+                return "value";
+            }
+
+            // DELETE api/<MoviesController>/5
+            [HttpDelete("{id}")]
+            public void Delete(int id)
+            {
+            }
+        }
+    }
+}
---
+++ Modul8_1302194032/Modul8_1302194032/Movies.cs
@@ -0,0 +1,10 @@
+﻿namespace Modul8_1302194032
+{
+    public class Movies
+    {
+        private string Title { get; set; }
+        private string Director { get; set; }
+        private List<string> Stars { get; set; }
+        private String Description { get; set; }
+    }
+}
